160e6b5ac9eff43cb7071ba0f4e2c9e2d7a9b3de
Semyon.Atamas <semyon.atamas@jetbrains.com>
2022-05-23 21:27:26 +0400

    Update annotation

--- shared/homeless/src/Controller/AppController.php
+++ shared/homeless/src/Controller/AppController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\ClientField;
 use App\Entity\ContractStatus;
 use App\Sonata\UserBundle\Entity\User;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
